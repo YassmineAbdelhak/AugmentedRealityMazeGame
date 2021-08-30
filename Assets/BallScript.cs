63178085e0d7b2b0aeed44d00d9ca5fa2f704476
Yasmine Abdelhak <yasmineabdelhak2014@gmail.com>
2021-08-30 15:52:45 +0200

    uploaded

---
+++ Assets/BallScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour {
+    public GameObject plane;
+    public GameObject spawnPoint;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(transform.position.y < plane.transform.position.y - 10) 
+        {
+            transform.position = spawnPoint.transform.position;
+        }
+		
+	}
+}
